b946f3e2b4e48e86ecf400ad1f65aad27f5a1054
Michael Nunes <tinysquid.git@gmail.com>
2020-04-13 15:30:10 -0700

    Add base graph functionality & initial BFT method

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,9 +3,11 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
+
 class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
 
@@ -13,26 +15,56 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        self.vertices[v1].add(v2)
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # Create a queue to get the 'breadth-first' functionality
+        # Create a dict to remember which nodes we've visited
+        # Enqueue starting_vertex
+        # From current vertex, enqueue connected vertices
+        # Mark current vertex and children as visited
+        # Repeat
+
+        visited = {}
+        queue = Queue()
+
+        # Enqueue starting vertex (root of traversal)
+        queue.enqueue(starting_vertex)
+        # Mark as visited
+        visited[starting_vertex] = True
+
+        # Continue until queue is empty (meaning the whole graph has been traversed)
+        while queue.size() != 0:
+            # Peek queue head
+            current_vertex = queue.queue[0]
+            # Enqueue connected vertices
+            for vertex in self.vertices[current_vertex]:
+                if not visited.get(vertex):
+                    # Add to queue if not already visited
+                    queue.enqueue(vertex)
+                    # Set as visited to prevent further enqueueing of same vertex
+                    visited[vertex] = True
+
+            # Dequeue current vertex
+            queue.dequeue()
+            # Do work on current vertex
+            print(current_vertex)
 
     def dft(self, starting_vertex):
         """
@@ -76,7 +108,8 @@ class Graph:
         """
         pass  # TODO
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     graph = Graph()  # Instantiate your graph
     # https://github.com/LambdaSchool/Graphs/blob/master/objectives/breadth-first-search/img/bfs-visit-order.png
     graph.add_vertex(1)
@@ -97,13 +130,13 @@ if __name__ == '__main__':
     graph.add_edge(2, 3)
     graph.add_edge(4, 6)
 
-    '''
+    """
     Should print:
         {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
-    '''
+    """
     print(graph.vertices)
 
-    '''
+    """
     Valid BFT paths:
         1, 2, 3, 4, 5, 6, 7
         1, 2, 3, 4, 5, 7, 6
@@ -117,29 +150,29 @@ if __name__ == '__main__':
         1, 2, 4, 3, 6, 5, 7
         1, 2, 4, 3, 7, 6, 5
         1, 2, 4, 3, 7, 5, 6
-    '''
+    """
     graph.bft(1)
 
-    '''
+    """
     Valid DFT paths:
         1, 2, 3, 5, 4, 6, 7
         1, 2, 3, 5, 4, 7, 6
         1, 2, 4, 7, 6, 3, 5
         1, 2, 4, 6, 3, 5, 7
-    '''
+    """
     graph.dft(1)
     graph.dft_recursive(1)
 
-    '''
+    """
     Valid BFS path:
         [1, 2, 4, 6]
-    '''
+    """
     print(graph.bfs(1, 6))
 
-    '''
+    """
     Valid DFS paths:
         [1, 2, 4, 6]
         [1, 2, 4, 7, 6]
-    '''
+    """
     print(graph.dfs(1, 6))
     print(graph.dfs_recursive(1, 6))
